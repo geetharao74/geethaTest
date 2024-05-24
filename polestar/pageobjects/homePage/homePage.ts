f82c85dce1319c1eb5aaebf75a9e71769287dfce
Isha-Rao <isharao.xx@gmail.com>
2024-05-24 13:32:01 +0530

    Added DialogFolder

--- polestar/pageobjects/homePage/homePage.ts
+++ polestar/pageobjects/homePage/homePage.ts
@@ -4,26 +4,21 @@ export class HomePage {
 
   readonly page: Page;
   readonly dropDownSelector: string;
-  readonly acceptButtonSelector: string;
   readonly returnHomeButtonSelector: string;
   readonly url ="https://www.polestar.com/se/developer/get-started/";
 
   constructor(page: Page) {
     this.page = page;
     this.dropDownSelector = 'a.css-kp9g7';
-    this.acceptButtonSelector = 'button#onetrust-accept-btn-handler';
     this.returnHomeButtonSelector = 'text=Return Home';
   }
 
   async waitUntilLoaded(){
-    await this.page.waitForSelector(this.acceptButtonSelector);  
+    await this.page.waitForSelector(this.dropDownSelector);  
   }
   async navigate(){
     await this.page.goto(this.url );
-  }
-
-  async clickAcceptCookieButton(){
-    await this.page.click(this.acceptButtonSelector);
+    await this.page.waitForLoadState();
   }
 
   async getReturnHomeButton(){
